9260c42c3cc29abcdd9fa6d17500badb67f0df66
poype <253186926@qq.com>
2017-10-14 10:53:01 +0800

    Server端的一个错误案例

--- src/com/poype/nio/Server.java
+++ src/com/poype/nio/Server.java
@@ -12,19 +12,30 @@ public class Server {
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         SocketAddress bindAddress = new InetSocketAddress("localhost",9999);
         serverSocketChannel.bind(bindAddress);
-        ByteBuffer buffer = ByteBuffer.allocate(10);
+        ByteBuffer recvBuffer = ByteBuffer.allocate(100);
+        ByteBuffer sendBuffer = ByteBuffer.allocate(100);
+        sendBuffer.put("Hello,Welcome".getBytes());
+        sendBuffer.flip();
         while (true) {
             SocketChannel socketChannel = serverSocketChannel.accept();
             System.out.println("接受一个连接");
-            int bytesRead = socketChannel.read(buffer);
+            int bytesRead = socketChannel.read(recvBuffer);
             while(bytesRead != -1) {
-                buffer.flip(); //一定要调用
-                while(buffer.hasRemaining()) {
-                    System.out.print((char)buffer.get());
+                recvBuffer.flip(); //一定要调用
+                while(recvBuffer.hasRemaining()) {
+                    System.out.print((char)recvBuffer.get());
                 }
-                buffer.clear(); //一定要调用
-                bytesRead = socketChannel.read(buffer);
+                recvBuffer.clear(); //一定要调用
+                //在网络中，下面这种调用是有问题的。因为如果客户端已经发送完了数据，那么服务端就会一直阻塞在这里
+                //可以对比一下微信，微信一次发送的消息长度是有一个最大值的。
+                //可以猜测后端有一块固定大小的buffer，客户端一次发送的数据量不能超过buffer的大小。
+                //除非定义复杂的通信协议，否则服务端无法知道客户端是否还有数据发送
+                bytesRead = socketChannel.read(recvBuffer);
+                System.out.println(bytesRead);
             }
+            System.out.println("send begin");
+            socketChannel.write(sendBuffer);
+            System.out.println("send end");
             socketChannel.close();
         }
     }
